5163f7a8abcec3b293c0ae9acf7e931952f105fb
ameera-jabar <ahabdeljabar@neiu.edu>
2024-11-12 00:52:40 +0000

    this is my final code

---
+++ hw3github new.R
@@ -0,0 +1,18 @@
+setwd("/cloud/project")
+
+TM <- read.csv("TextMessages.csv", header = TRUE) 
+is.factor(TM$Six_months)
+#False
+
+names(TM)
+
+install.packages("pastecs")
+
+library(pastecs)
+
+stat.desc(TM$Baseline)
+
+stat.desc(TM$Six_months)
+
+round(stat.desc(TM$Baseline),2)
+
